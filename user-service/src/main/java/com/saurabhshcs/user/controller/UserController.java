ae60bc70b03f78c13927bff23d310140fae5d941
saurabhshcs <saurabhshcs@gmail.com>
2022-11-18 00:44:44 +0000

    Modify controller for test base URL

--- user-service/src/main/java/com/saurabhshcs/user/controller/UserController.java
+++ user-service/src/main/java/com/saurabhshcs/user/controller/UserController.java
@@ -2,16 +2,18 @@ package com.saurabhshcs.user.controller;
 
 import com.saurabhshcs.user.domain.User;
 import com.saurabhshcs.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
-
 @RestController
+@Slf4j
 public class UserController
 {
-    @Resource
+    @Autowired
     private UserService userService;
 
     @PostMapping("/createUser")
@@ -19,4 +21,11 @@ public class UserController
         return userService.createUser(user);
     }
 
+    @GetMapping("/getuser")
+    public String getUser(){
+        final String name = "saurabh";
+        log.info("Returning User:{}", name);
+        return name;
+
+    }
 }
--- user-service/src/main/java/com/saurabhshcs/user/service/UserService.java
+++ user-service/src/main/java/com/saurabhshcs/user/service/UserService.java
@@ -2,9 +2,11 @@ package com.saurabhshcs.user.service;
 
 import com.saurabhshcs.user.domain.User;
 import com.saurabhshcs.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
+@Service
 public class UserService
 {
     @Resource
